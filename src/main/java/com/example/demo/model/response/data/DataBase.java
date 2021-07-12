18bd566f05a03d7670bcccd2f645a6630b27dac2
manishsrivastava78 <manishsrivastava78@gmail.com>
2021-07-12 09:12:56 +0530

    Update DataBase.java

--- src/main/java/com/example/demo/model/response/data/DataBase.java
+++ src/main/java/com/example/demo/model/response/data/DataBase.java
@@ -57,7 +57,7 @@ public class DataBase {
 		emp.setFirstName("Test1");
 		emp.setLastName("Larsen");
 		emp.setDepartment("admin");
-		emp.setEmpId("886");
+		emp.setEmpId("786");
 		emp.setLocation("London");
 		employees.add(emp);
 		
