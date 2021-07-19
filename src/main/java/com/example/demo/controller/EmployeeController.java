d329624ce12d4f9f1f6aaf7a04aa6cb7499185f9
manishsrivastava78 <manishsrivastava78@gmail.com>
2021-07-19 09:27:30 +0530

    Add logs

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,17 +20,25 @@ import com.example.demo.model.response.exception.NotFound;
 @RestController
 @RequestMapping("employees")
 public class EmployeeController {
+	/**
+	 * Logger object
+	 */
+	private static final Logger logger = LogManager.getLogger(EmployeeController.class);
 	
 	@GetMapping(produces= {MediaType.APPLICATION_JSON_VALUE})
 	public @ResponseBody ResponseEntity<List<Employee>> getAll() {
+		logger.info("Getting list of all employees...");
 	    return new ResponseEntity<List<Employee>>(DataBase.getEmployees(), HttpStatus.OK);
 	}
 	
 	@GetMapping(path="/{empId}")
 	public Employee getUserById(@PathVariable String empId) {
+		logger.info("Search employee id "+empId+" in the system...");
 		Employee e  =DataBase.getEmployeeById(empId);
-		if(e == null)
+		if(e == null) {
+			logger.info("Employee id "+empId+" does not exist in the system...");
 			throw new NotFound();
+		}
 		return e;
 		}
 		
--- src/main/java/com/example/demo/model/response/exception/NotFound.java
+++ src/main/java/com/example/demo/model/response/exception/NotFound.java
@@ -6,4 +6,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code= HttpStatus.NOT_FOUND, reason = "{\"message\":\"Employee does not exist\"}")
 public class NotFound extends RuntimeException{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1981175626825036512L;
+
 }
